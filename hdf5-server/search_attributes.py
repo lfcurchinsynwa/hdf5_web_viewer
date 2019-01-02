e0919ab8ef9cd83d084bb6ef5ede7e55952215cd
Sandeep Subedi <theurchinsynwa@gmail.com>
2019-01-02 23:25:34 +0100

    search multiple folders and seperate paths from different files

--- hdf5-server/search_attributes.py
+++ hdf5-server/search_attributes.py
@@ -12,49 +12,50 @@ context = SSL.Context(SSL.SSLv23_METHOD)
 
 app = Flask(__name__)
 
-data_dir = r"C:\wamp64\www\sfb805\hdf5-files"
+data_dir = [r"C:\wamp64\www\sfb805\hdf5-files", r"C:\wamp64\www\sfb805\hdf5-files-alt"]
 
 # return hdf5 files from the root folder
 @app.route('/getFiles', methods=['POST'])
 def get_files():
   files = []
-  for filename in os.listdir(data_dir):
-    if filename.endswith(".h5"):
-      files.append(filename)
+  for i in data_dir:
+    for filename in os.listdir(i):
+      if filename.endswith(".h5"):
+        files.append(filename)
   return(jsonify({"files": files }))
 
 # get the child of each group and subgroup
 @app.route('/getKeys', methods=['POST'])
 def api_getKeys():
-  print("getkey starting at:", time.gmtime())
   filename = request.form['filename']
-  f = h5py.File(os.path.join(data_dir, filename), "r")
-  if(request.form['path']):
-    path = request.form['path']
-    if isinstance(f[path], h5py.Dataset):
-      return(path)
-    else:
-      data = []
-      for key in f[path].keys():
-        obj = {
-          'id' : key,
-          'path': path + '/' + key,
-          'type': 'group'
-        }
-        data.append(obj)
-      print("getkey end at:", time.gmtime())
-      return(jsonify(data))
-  else:
-    data = []
-    print("items:", f.items())
-    for key in f.keys():
-      obj = {
-        'id' : key,
-        'path': '/' + key
-      }
-      data.append(obj)
-    print("getkey end at:", time.gmtime())
-    return(jsonify(data)) 
+  for i in data_dir:
+    try:
+      f = h5py.File(os.path.join(i, filename), "r")
+      if(request.form['path']):
+        path = request.form['path']
+        if isinstance(f[path], h5py.Dataset):
+          return(path)
+        else:
+          data = []
+          for key in f[path].keys():
+            obj = {
+              'id' : key,
+              'path': path + '/' + key,
+              'type': 'group'
+            }
+            data.append(obj)
+          return(jsonify(data))
+      else:
+        data = []
+        for key in f.keys():
+          obj = {
+            'id' : key,
+            'path': '/' + key
+          }
+          data.append(obj)
+        return(jsonify(data))
+    except:
+      pass
 
 # get the paths of searched attributes
 @app.route('/getpath', methods=['POST'])
@@ -75,7 +76,7 @@ def api_gethdf():
     if search_type == 'OR':
       for attribute,value in attributes.items():
         if attribute in object.attrs and object.attrs[attribute] == value:
-          paths.append(name)
+          paths.append(("/".join(os.path.normpath(object.file.filename).split(os.sep)[-1:])) + '/' + name)
           filepath.append("/".join(os.path.normpath(object.file.filename).split(os.sep)[-1:]))
     elif search_type == 'AND':
       allmatch = True
@@ -86,23 +87,27 @@ def api_gethdf():
           allmatch = False
           break
       if allmatch:
-        paths.append(name)
+        paths.append(("/".join(os.path.normpath(object.file.filename).split(os.sep)[-1:])) + '/' + name)
         filepath.append("/".join(os.path.normpath(object.file.filename).split(os.sep)[-1:]))
     else :
       first_attr_name = attributes.keys()[0]
       first_attr_value = attributes[first_attr_name]
       if first_attr_name in object.attrs and object.attrs[first_attr_name] == first_attr_value:
-        paths.append(name)
+        paths.append(("/".join(os.path.normpath(object.file.filename).split(os.sep)[-1:])) + '/' + name)
         filepath.append("/".join(os.path.normpath(object.file.filename).split(os.sep)[-1:]))
-      
       for attr_name in attributes.keys()[1:]: 
         attr_value = attributes[attr_name]
         if attr_name in object.attrs and object.attrs[attr_name] == attr_value:
           for path in paths:
-            if path in name:
-              connectedPaths.append(name)
+            totpath = ("/".join(os.path.normpath(object.file.filename).split(os.sep)[-1:]) + '/' + name)
+            if path in totpath:
+              connectedPaths.append(("/".join(os.path.normpath(object.file.filename).split(os.sep)[-1:])) + '/' + name)
               break
-          paths.append(name)
+          paths.append(("/".join(os.path.normpath(object.file.filename).split(os.sep)[-1:])) + '/' + name)
+
+
+
+
       # for i, (attribute, value) in  enumerate (attributes.items()):
       #   if attribute in object.attrs and object.attrs[attribute] == value:
       #     paths.append(name)
@@ -112,12 +117,12 @@ def api_gethdf():
       #       connectedPaths.append(name)
 
 
-
-  for filename in os.listdir(data_dir):
-    if filename.endswith(".h5"):
-      f = h5py.File(os.path.join(data_dir, filename), "r")
-      f.visititems(get_path)
-  result = jsonify({"paths": paths, "connectedPaths": connectedPaths, "searchType": search_type, "filePath": filepath})
+  for i in data_dir:
+    for filename in os.listdir(i):
+      if filename.endswith(".h5"):
+        f = h5py.File(os.path.join(i, filename), "r")
+        f.visititems(get_path)
+  result = jsonify({"filePath": list(set(filepath)), "paths": paths, "connectedPaths": connectedPaths, "searchType": search_type})
   return result
 
 # get attributes while clicked on js tree
@@ -126,25 +131,27 @@ def api_getattr():
   attrs = ''
   tmp_path = request.form['path']
   fileName = request.form['fileName']
-  for filename in os.listdir(data_dir):
-    if filename == fileName:
-      f = h5py.File(os.path.join(data_dir, filename), "r")
-      try:
-        attrs = list(f[tmp_path].attrs.items())
-        
-      except:
-        print("nothing found")
+  for i in data_dir:
+    for filename in os.listdir(i):
+      if filename == fileName:
+        f = h5py.File(os.path.join(i, filename), "r")
+        try:
+          attrs = list(f[tmp_path].attrs.items())
+        except:
+          print("nothing found")
   return jsonify(attrs)
 
 # get the contents of dataframe
 @app.route('/fileContents', methods=['POST'])
 def api_getContent():
-  filename = request.form['filename']
+  fileName = request.form['filename']
   path = request.form['path']
-  f = h5py.File(os.path.join(data_dir, filename), "r")
+  for i in data_dir:
+    for filename in os.listdir(i):
+      if fileName == filename:
+        f = h5py.File(os.path.join(i, filename), "r")
   dset = f[path]
   data = dset[()]
-  print(data)
   dataframe = pd.DataFrame(data)
   return dataframe.to_html(classes=["table", "table-bordered", "table-striped", "table-hover"])
   # return 'OK'
